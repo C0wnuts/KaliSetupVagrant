5b9bbd4e8d8addf14658911d133c4faf3ae8729a
kevin.racca <kevin.racca@fidens.fr>
2021-02-15 15:07:29 +0100

    fix installation tools and add some

--- init.sh
+++ init.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
+TIMEZONE="Europe/Paris"
+
 function gitCloneBulk()
 {
 	git clone https://github.com/superkojiman/onetwopunch.git /opt/onetwopunch
-	git clone https://github.com/drwetter/testssl.sh.git /opt/testssl
-	git clone https://github.com/rastating/joomlavs.git /opt/joomlavs
-	git clone https://github.com/aboul3la/Sublist3r.git /opt/Sublist3r
-	git clone https://github.com/trustedsec/unicorn /opt/unicorn
-	git clone https://github.com/Screetsec/Sudomy.git /opt/Sudomy
-	git clone https://github.com/m8r0wn/crosslinked /opt/crosslinked
-	git clone https://github.com/maurosoria/dirsearch.git /opt/dirsearch
+	git clone https://github.com/21y4d/nmapAutomator.git /opt/nmapAutomator
+	git clone https://github.com/vulnersCom/nmap-vulners.git /opt/nmap-vulners
+
+	git clone https://github.com/drwetter/testssl.sh.git /opt/web/testssl
+	git clone https://github.com/rastating/joomlavs.git /opt/web/joomlavs
+	git clone https://github.com/aboul3la/Sublist3r.git /opt/web/Sublist3r
+	git clone https://github.com/Screetsec/Sudomy.git /opt/web/Sudomy
+	git clone https://github.com/maurosoria/dirsearch.git /opt/web/dirsearch
+	git clone https://github.com/C0wnuts/webExploitPages.git /opt/web/webExploitPages
+	git clone https://github.com/internetwache/GitTools.git /opt/web/gitTools
+	git clone https://github.com/ticarpi/jwt_tool.git /opt/web/jwt_tool
+	git clone https://github.com/00theway/Ghostcat-CNVD-2020-10487.git /opt/web/Ghostcat
+	git clone https://github.com/immunIT/drupwn.git /opt/web/drupwn
+	git clone https://github.com/anouarbensaad/vulnx.git /opt/web/vulnx
+
+	git clone https://github.com/trustedsec/unicorn.git /opt/internal/unicorn
+	git clone https://github.com/SecureAuthCorp/impacket.git /opt/internal/impacket
+	
+	git clone https://github.com/m8r0wn/CrossLinked.git /opt/osint/crosslinked
+	git clone https://github.com/initstring/linkedin2username.git /opt/osint/linkedin2username
+
 	git clone https://github.com/k4m4/dymerge.git /opt/wordlist/dymerge
-	git clone https://github.com/1N3/IntruderPayloads /opt/wordlist/IntruderPayloads
+	git clone https://github.com/1N3/IntruderPayloads.git /opt/wordlist/IntruderPayloads
 	git clone https://github.com/C0wnuts/FinalDics.git /opt/wordlist/FinalDics
 	git clone https://github.com/fuzzdb-project/fuzzdb.git /opt/wordlist/fuzzdb
 	git clone https://github.com/danielmiessler/SecLists.git /opt/wordlist/SecList
-	git clone https://github.com/byt3bl33d3r/DeathStar /opt/DeathStar
+
 	git clone https://github.com/mzet-/linux-exploit-suggester.git /opt/privEscTools/linux-exploit-suggester-sh
 	git clone https://github.com/jondonas/linux-exploit-suggester-2.git /opt/privEscTools/linux-exploit-suggester-pl
 	git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/privEscTools/privilege-escalation-awesome-scripts-suite
@@ -25,14 +41,6 @@ function gitCloneBulk()
 	git clone https://github.com/sleventyeleven/linuxprivchecker.git /opt/privEscTools/linuxprivchecker
 	git clone https://github.com/pentestmonkey/unix-privesc-check.git /opt/privEscTools/unix-privesc-check
 	git clone https://github.com/TH3xACE/SUDO_KILLER.git /opt/privEscTools/SUDO_KILLER
-	git clone https://github.com/C0wnuts/webExploitPages.git /opt/webExploitPages
-	git clone https://github.com/internetwache/GitTools.git /opt/gitTools
-	git clone https://github.com/ticarpi/jwt_tool /opt/jwt_tool
-	git clone https://github.com/21y4d/nmapAutomator.git /opt/nmapAutomator
-	git clone https://github.com/vulnersCom/nmap-vulners.git /opt/nmap-vulners
-	git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
-	git clone https://github.com/lgandx/Responder.git /opt/Responder
-	git clone https://github.com/00theway/Ghostcat-CNVD-2020-10487.git /opt/Ghostcat
 }
 
 function runAptGetUpgrade()
@@ -49,17 +57,25 @@ function runAptGetInstall()
     DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install $1
 }
 
+function setupVulnx()
+{
+	cd /opt/web/vulnx
+	chmod +x ./install.sh
+	./install.sh
+	cd /opt
+}
+
 function pipInstallBulk()
 {
-	pip install pipenv
-	pip3 install pipenv
-	pip3 install pycryptodomex
-	pip3 install -r /opt/crosslinked/requirements.txt
-	pip install -r /opt/Sublist3r/requirements.txt
-	pip install -r /opt/Sudomy/requirements.txt
-	pip3 install -r /opt/Sudomy/requirements.txt
-	pip3 install -r /opt/DeathStar/requirements.txt
-	pip3 install droopescan
+	pip3 install pipenv pycryptodomex termcolor cprint pycryptodomex requests colorama droopescan
+	pip3 install pylnk3 --force
+	pip3 install -r /opt/osint/crosslinked/requirements.txt --force
+	pip3 install -r /opt/osint/linkedin2username/requirements.txt --force
+	pip3 install -r /opt/web/Sublist3r/requirements.txt --force
+	pip3 install -r /opt/web/Sudomy/requirements.txt --force
+	pip3 install -r /opt/web/drupwn/requirements.txt --force
+	pip3 install -r /opt/web/dirsearch/requirements.txt --force
+	pip3 install -r /opt/internal/impacket/requirements.txt --force
 }
 
 function changeKeyboard()
@@ -93,14 +109,13 @@ function installPhantomjs()
 	runAptGetInstall libfreetype6 libfreetype6-dev
 	runAptGetInstall libfontconfig1 libfontconfig1-dev
 	cd ~
-	export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
-	wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
-	tar xvjf $PHANTOM_JS.tar.bz2
-	mv $PHANTOM_JS /usr/local/share
-	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
-	phantomjs --version
+	wget https://github.com/Medium/phantomjs/archive/2.1.14.tar.gz
+	tar xvf 2.1.14.tar.gz
+	mv phantomjs-2.1.14 /usr/local/share/
+	ln -sf /usr/local/share/phantomjs-2.1.14/bin/phantomjs /usr/local/bin
 	echo 'export OPENSSL_CONF=/etc/ssl/' >> /root/.bashrc
 	echo 'export OPENSSL_CONF=/etc/ssl/' >> /home/vagrant/.bashrc
+	rm 2.1.14.tar.gz
 }
 
 function zshSetup()
@@ -132,8 +147,7 @@ function zshSetup()
 
 function nmapAutomatorSetup()
 {
-	cd /opt/nmapAutomator
-	chmod +x ./nmapAutomator.sh
+	chmod +x /opt/nmapAutomator.sh
 	cd /opt/nmap-vulners
 	cp *.nse /usr/share/nmap/scripts
 	nmap --script-updatedb
@@ -144,17 +158,8 @@ function nmapAutomatorSetup()
 function symbolicLinkSetup()
 {
 	ln -s /opt/nmapAutomator/nmapAutomator.sh /usr/bin/nmapAutomator
-	ln -s /opt/joomlavs/joomlavs.rb /usr/bin/joomlavs
-	ln -s /opt/testssl/testssl.sh /usr/bin/testssl
-}
-
-function crackMapExec()
-{
-	apt remove -y crackmapexec
-	python3 -m pip install pipx
-	pipx ensurepath
-	pipx install crackmapexec --force
-	pipx ensurepath
+	ln -s /opt/web/joomlavs/joomlavs.rb /usr/bin/joomlavs
+	ln -s /opt/web/testssl/testssl.sh /usr/bin/testssl
 }
 
 function SimpleHTTPServerGolang()
@@ -163,6 +168,19 @@ function SimpleHTTPServerGolang()
 	cd /opt/SimpleHTTPServer-golang
 	wget https://github.com/nodauf/SimpleHTTPServer-golang/releases/download/v0.5/webserver-linux-amd64
 	chmod +x webserver-linux-amd64
+}
+
+function deathStar()
+{
+	python3 -m pip install pipx
+	python3 -m pipx install deathstar-empire
+	python3 -m pipx ensurepath
+	sudo python3 -m pipx ensurepath
+}
+
+function initopt()
+{
+	mkdir /opt /opt/web /opt/internal /opt/privEscTools /opt/wordlist /opt/osint
 }
 
 i=0
@@ -177,40 +195,38 @@ do
 	fi
 done
 
+timedatectl set-timezone $TIMEZONE
 sleep 10
 echo "Kill apt process"
 killall apt apt-get > /dev/null 2>&1
 echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
 echo "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
-
 unset UCF_FORCE_CONFFOLD
 export UCF_FORCE_CONFFNEW=YES
 ucf --purge /boot/grub/menu.lst
-
 export DEBIAN_FRONTEND=noninteractive
-
 apt-get update
 runAptGetUpgrade
-runAptGetInstall "aha xalan geany golang jq gdebi tree keepass2 gcc-9-base gcc python3-pip remmina"
+runAptGetInstall "aha xalan geany golang jq gdebi tree keepass2 gcc-9-base gcc python3-pip remmina npm chromium parallel moreutils"
+npm chromium parallel
 installPhantomjs
-mkdir /opt
+initopt
 cd /opt
 wget https://github.com/gnunn1/tilix/releases/download/1.9.3/tilix.zip
 sudo unzip tilix.zip -d /
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 rm tilix.zip
-mkdir /opt/wordlist
-mkdir /opt/privEscTools
 gitCloneBulk
-cd /opt/wordlist
+deathStar
 gzip -d /usr/share/wordlists/rockyou.txt.gz
-mv /usr/share/wordlists/rockyou.txt ./
-cd /opt/joomlavs/
+cp /usr/share/wordlists/rockyou.txt /opt/wordlist/
+cd /opt/web/joomlavs/
 runAptGetInstall "libssl-dev libffi-dev python-dev build-essential patch ruby-dev zlib1g-dev liblzma-dev libcurl4-openssl-dev python3-venv"
 pip install wheel
 bundle update --bundler
 gem install bundler && bundle install
 pipInstallBulk
+setupVulnx
 systemctl start postgresql
 systemctl enable postgresql
 msfdb init
@@ -220,9 +236,8 @@ go build -o dnssearch main.go
 go get -u github.com/tomnomnom/httprobe
 go get -u github.com/OJ/gobuster
 SimpleHTTPServerGolang
-crackMapExec
 nmapAutomatorSetup
-runAptGetInstall "apt-transport-https ca-certificates curl gnupg2 software-properties-common enum4linux powershell-empire"
+runAptGetInstall "apt-transport-https ca-certificates curl gnupg2 software-properties-common enum4linux powershell-empire ffuf"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
 apt-get update
